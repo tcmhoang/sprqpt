1dacfa7d17a53cfa0d82c9f2e8de52ec67670f13
Conrad Hoang <tcmhoang@outlook.com>
2023-10-15 09:50:52 -0400

    style: :star:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,32 @@
 <template lang="pug">
 .scene
 	.rainbow
-		.sprite
+	.starfield
+		.star
+
 </template>
 
 <style lang="scss">
 	$colors: ($red, $peach, $yellow, $green, $sky, $mauve);
 	$ncolor: length($colors);
+	$unit: calc(100% / $ncolor);
 
-	@function _colorTile($color) {
-		@return (_genTile($color, transparent), _genTile(transparent, $color));
+	@function _colorTransTiles($color) {
+		@return (_gen2StopTile($color, transparent), _gen2StopTile(transparent, $color));
 	}
 
-	@function _genTile($c1, $c2) {
+	@function _gen2StopTile($c1, $c2) {
 		@return linear-gradient(to right, $c1 0%, $c1 50%, $c2 50%, $c2 100%);
+	}
+
+	@function _genColoredTile($c) {
+		@return linear-gradient(to right, $c 0%, $c 100%);
 	}
 
 	@function render_rainbow() {
 		$res: ();
 		@each $c in $colors {
-			$res: append($res, _colorTile($c), comma);
+			$res: append($res, _colorTransTiles($c), comma);
 		}
 		@return $res;
 	}
@@ -36,6 +43,7 @@
 		aspect-ratio: 3/1;
 		position: relative;
 		margin: 0;
+		background-color: red;
 	}
 
 	.rainbow {
@@ -43,19 +51,111 @@
 		position: absolute;
 		left: 0;
 		right: 50%;
-		top: $padding / 2;
+		top: calc($padding / 2);
 		height: 100% - $padding;
+
+		&::after {
+			content: '';
+			position: inherit;
+			left: 0;
+			right: $unit;
+			top: 0;
+			bottom: 0;
+			background-image: render_rainbow();
+			background-position: render_positions(calc($unit/2));
+			background-size: calc($unit * 3) $unit;
+			background-repeat: repeat-x;
+		}
 	}
-	.rainbow .sprite {
-		$span: calc(100% / ($ncolor * 2));
-		position: inherit;
-		left: 0;
-		right: 80px;
-		top: 0;
-		bottom: 0;
-		background-image: render_rainbow();
-		background-position: render_positions($span);
-		background-size: calc(100% / ($ncolor / 3)) calc(100% / $ncolor);
-		background-repeat: repeat-x;
+
+	.starfield {
+		height: 100%;
+		width: 100%;
+		position: relative;
+	}
+
+	.star {
+		height: calc($unit * 2);
+		aspect-ratio: 1;
+		position: absolute;
+		overflow: visible;
+
+		&::after {
+			$factor: 7;
+			content: '';
+			overflow: visible;
+			display: block;
+			width: calc(100% * $factor);
+			height: 100%;
+			// 1st: 1, 2nd: 4, 3rd: 4, 4th: 5, 5th: 8, 6th: 4 => tot : 26
+			$totsprites: 26;
+			$sprites: ();
+			@for $_ from 1 through $totsprites {
+				$sprites: append($sprites, _genColoredTile($text), comma);
+			}
+			background-image: $sprites;
+			@function factor_position($e, $x, $y) {
+				@return calc(100% / $factor * $e + $x / $factor) $y;
+			}
+
+			$poss: (
+				((40%, 50%)),
+				((30%, 50%), (50%, 50%), (40%, 40%), (40%, 60%)),
+				((20%, 50%), (57%, 50%), (40%, 30%), (40%, 68%)),
+				((50%, 50%), (25%, 50%), (75%, 50%), (50%, 25%), (50%, 75%)),
+				(
+					(0%, 50%),
+					(100%, 50%),
+					(50%, 0%),
+					(50%, 100%),
+					(15%, 15%),
+					(85%, 85%),
+					(85%, 15%),
+					(15%, 85%)
+				),
+				((0%, 50%), (100%, 50%), (50%, 0%), (50%, 100%))
+			);
+			$transformed_poss: ();
+
+			@for $i from 1 through length($poss) {
+				// fix for weird bug :))
+				@if $i == 1 {
+					$temp: nth($poss, $i);
+					$transformed_poss: append(
+						$transformed_poss,
+						factor_position($i, nth($temp, 1), nth($temp, 2)),
+						comma
+					);
+				} @else {
+					@each $p in nth($poss, $i) {
+						$transformed_poss: append(
+							$transformed_poss,
+							factor_position($i, nth($p, 1), nth($p, 2)),
+							comma
+						);
+					}
+				}
+			}
+
+			background-position: $transformed_poss;
+			$w: calc(10% / 7);
+			$dw: calc($w * 2);
+			$szs: (
+				(#{$w} 10%),
+				(#{$w} 10%, #{$w} 10%, #{$w} 10%, #{$w} 10%),
+				(#{$dw} 10%, #{$dw} 10%, #{$w} 20%, #{$w} 20%),
+				(#{$w} 10%, #{$dw} 10%, #{$dw} 10%, #{$w} 20%, #{$w} 20%),
+				(#{$w} 10%, #{$w} 10%, #{$w} 10%, #{$w} 10%, #{$w} 10%, #{$w} 10%),
+				(#{$w} 10%, #{$w} 10%, #{$w} 10%, #{$w} 10%)
+			);
+
+			$flatten_szs: ();
+			@each $sz in $szs {
+				$flatten_szs: append($flatten_szs, $sz, comma);
+			}
+
+			background-size: $flatten_szs;
+			background-repeat: no-repeat;
+		}
 	}
 </style>
