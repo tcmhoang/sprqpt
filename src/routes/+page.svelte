a043a21f6b7244a4f305c0b0dad1d4e334baf3e0
Conrad Hoang <tcmhoang@outlook.com>
2023-10-17 08:07:10 -0400

    refactor: :smiley_cat:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,51 +2,28 @@
 	import Nyan from './Nyan.svelte';
 </script>
 
-<template lang="pug">
-.scene
-	.rainbow
-	.starfield
-		.star
-	.cat
-		Nyan
-</template>
+<div class="scene">
+	<div class="rainbow" />
+	<div class="starfield">
+		<div class="star" />
+	</div>
+	<div class="cat">
+		<Nyan />
+	</div>
+</div>
 
 <style lang="scss">
 	$colors: ($red, $peach, $yellow, $green, $sky, $mauve);
-	$ncolor: length($colors);
-	$unit: calc(100% / $ncolor);
+	$star_color: $text;
+
 	$padding: 40%;
 	$height: calc(100% - $padding);
+	$ncolor: length($colors);
 
-	@function _colorTransTiles($color) {
-		@return (_gen2StopTile($color, transparent), _gen2StopTile(transparent, $color));
-	}
-
-	@function _gen2StopTile($c1, $c2) {
-		@return linear-gradient(to right, $c1 0%, $c1 50%, $c2 50%, $c2 100%);
-	}
-
-	@function _genColoredTile($c) {
-		@return linear-gradient(to right, $c 0%, $c 100%);
-	}
-
-	@function render_rainbow() {
-		$res: ();
-		@each $c in $colors {
-			$res: append($res, _colorTransTiles($c), comma);
-		}
-		@return $res;
-	}
-
-	@function render_positions($span) {
-		$res: ();
-		@for $i from 0 through (length($colors) * 2) {
-			$res: append($res, 0 $i * $span, comma);
-		}
-		@return $res;
-	}
+	$unit: calc(100% / $ncolor);
 
 	.scene {
+		--speed: var(--speed, 0.5);
 		width: 100%;
 		aspect-ratio: 3/1;
 		position: relative;
@@ -54,6 +31,33 @@
 	}
 
 	.rainbow {
+		@function _gen2StopTile($c1, $c2) {
+			@return linear-gradient(to right, $c1 0%, $c1 50%, $c2 50%, $c2 100%);
+		}
+
+		@function render_rainbow() {
+			$res: ();
+			@each $c in $colors {
+				$res: append(
+					$res,
+					(
+						_gen2StopTile($c, transparent),
+						_gen2StopTile(transparent, $c)
+					),
+					comma
+				);
+			}
+			@return $res;
+		}
+
+		@function render_positions($span) {
+			$res: ();
+			@for $i from 0 through (length($colors) * 2) {
+				$res: append($res, 0 $i * $span, comma);
+			}
+			@return $res;
+		}
+
 		position: absolute;
 		left: 0;
 		right: 50%;
@@ -88,8 +92,8 @@
 
 		&::after {
 			$factor: 6;
-			position: absolute;
 			content: '';
+			position: inherit;
 			display: block;
 			width: calc(100% * $factor);
 			height: 100%;
@@ -98,7 +102,11 @@
 			$totsprites: 26;
 			$sprites: ();
 			@for $_ from 1 through $totsprites {
-				$sprites: append($sprites, _genColoredTile($text), comma);
+				$sprites: append(
+					$sprites,
+					linear-gradient(to right, $star_color 0%, $star_color 100%),
+					comma
+				);
 			}
 
 			background-image: $sprites;
@@ -124,6 +132,7 @@
 				((10%, 50%), (90%, 50%), (50%, 10%), (50%, 90%))
 			);
 			$transformed_poss: ();
+
 			@for $i from 1 through length($poss) {
 				// fix for weird bug :))
 				@if $i == 1 {
@@ -164,35 +173,22 @@
 
 			background-size: $flatten_szs;
 			background-repeat: no-repeat;
-			animation: 1s step-end infinite both sparks;
+			animation: calc(var(--speed, 0.5) * 2 * 1s) step-end infinite spark;
 
 			@mixin move_to($frame) {
 				transform: translateX(calc(-100% * $frame / $factor));
 			}
 
-			@keyframes sparks {
+			@keyframes spark {
 				from,
 				to {
 					@include move_to(6);
 				}
 
-				50% {
-					@include move_to(0);
-				}
-				58.3% {
-					@include move_to(1);
-				}
-				66.7% {
-					@include move_to(2);
-				}
-				75% {
-					@include move_to(3);
-				}
-				83.3% {
-					@include move_to(4);
-				}
-				91.7% {
-					@include move_to(5);
+				@for $i from 0 to 6 {
+					#{calc(50% + 50% * ($i / 6))} {
+						@include move_to($i);
+					}
 				}
 			}
 		}
