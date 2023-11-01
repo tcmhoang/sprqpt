3877eeab11d3e4a5ae3c4c52aee034b71c0f1e36
Conrad Hoang <tcmhoang@outlook.com>
2023-11-01 00:42:17 -0400

    feat: sketch out the structure for cheep

---
+++ src/routes/(site)/(main)/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Cheep from './Cheep.svelte';
+</script>
+
+<div>
+	<Cheep />
+</div>
+
+<style>
+	div {
+		display: flex;
+		gap: 1rem;
+		flex-flow: column nowrap;
+	}
+</style>
---
+++ src/routes/(site)/(main)/Cheep.svelte
@@ -0,0 +1,21 @@
+<article class="cheep">
+	<a href="/" aria-label="Conrad's Feed">
+		<img
+			src="https://avatars.githubusercontent.com/u/51996720?s=80&v=4"
+			alt="Author"
+			width="80"
+			height="80"
+			decoding="async"
+			loading="lazy"
+		/>
+	</a>
+	<div class="content">
+		<div class="author">
+			<a href="/" class="name">Conrad</a>
+			<span>•</span>
+			<time datetime="2008-02-14 20:00">Formatted Date</time>
+		</div>
+		<div class="status">Feeling Okay</div>
+		<div class="content">Random content</div>
+	</div>
+</article>
