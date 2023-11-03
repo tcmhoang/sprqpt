ff6236d18b4cde9d956a53d87824e6646bb43d99
Conrad Hoang <tcmhoang@outlook.com>
2023-11-03 00:41:59 -0400

    chore: update config mdsvex

---
+++ src/routes/(site)/(main)/about/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import About from '$lib/contents/about.md';
+</script>
+
+<About/>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 const config = {
 	kit: {
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md']
+		})
+	]
 };
 
 export default config;
