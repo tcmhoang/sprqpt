30a681e2d50c7014de7e67fc8d9b22bc8516deec
Conrad Hoang <tcmhoang@outlook.com>
2023-12-06 23:58:49 -0500

    feat: add my dev shell config

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A nix flake for building my blog";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in
+    with pkgs; {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+
+      devShells.x86_64-linux.default = mkShell {
+	nativeBuildInputs = [nodejs_21];
+        buildInputs = [
+	corepack_21
+	nodePackages_latest.svelte-check
+	nodePackages_latest.svelte-language-server
+	vscode-langservers-extracted
+	fish
+	];
+	shellHook = ''
+		fish
+		pnpm i
+	'';
+      };
+    };
+}
