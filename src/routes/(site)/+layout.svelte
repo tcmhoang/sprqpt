ff32fb80f1e0b4329030f2b96a00e1919df42227
Conrad Hoang <tcmhoang@outlook.com>
2023-12-17 08:35:01 -0500

    refactor: move footer to another component

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -6,6 +6,7 @@
 	import CatIcon from '$lib/icons/CatIcon.svelte';
 	import { page } from '$app/stores';
 	import default_banner from '$lib/constants/default_banner';
+	import Footer from './Footer.svelte';
 
 	let hide_seek = false;
 
@@ -40,16 +41,7 @@
 
 <main class="w-content main"><slot /></main>
 
-<div class="hidden" aria-hidden="true" itemtype="http://schema.org/Person">
-	<span itemprop="gender">Male</span>
-	<span itemprop="email">tcmhoang@outlook.com</span>
-	<span itemprop="givenName">Tuan</span>
-	<span itemprop="familyName">Hoang</span>
-	<span itemprop="name">Tuan Minh Hoang</span>
-	<span itemprop="additionalName">Conrad Hoang</span>
-	<span itemprop="jobTitle">Software Engineer</span>
-	<span itemprop="nationality">VN</span>
-</div>
+<Footer />
 
 <style>
 	.wm-cat {
---
+++ src/routes/(site)/Footer.svelte
@@ -0,0 +1,10 @@
+<div class="hidden" aria-hidden="true" itemtype="http://schema.org/Person">
+	<span itemprop="gender">Male</span>
+	<span itemprop="email">tcmhoang@outlook.com</span>
+	<span itemprop="givenName">Tuan</span>
+	<span itemprop="familyName">Hoang</span>
+	<span itemprop="name">Tuan Minh Hoang</span>
+	<span itemprop="additionalName">Conrad Hoang</span>
+	<span itemprop="jobTitle">Software Engineer</span>
+	<span itemprop="nationality">VN</span>
+</div>
