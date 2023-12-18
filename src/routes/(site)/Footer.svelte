fc90c028971542b38dfbd1f1d793edad9944b720
Conrad Hoang <tcmhoang@outlook.com>
2023-12-18 10:41:36 -0500

    change footer

--- src/routes/(site)/Footer.svelte
+++ src/routes/(site)/Footer.svelte
@@ -1,10 +1,47 @@
-<div class="hidden" aria-hidden="true" itemtype="http://schema.org/Person">
-	<span itemprop="gender">Male</span>
-	<span itemprop="email">tcmhoang@outlook.com</span>
-	<span itemprop="givenName">Tuan</span>
-	<span itemprop="familyName">Hoang</span>
-	<span itemprop="name">Tuan Minh Hoang</span>
-	<span itemprop="additionalName">Conrad Hoang</span>
-	<span itemprop="jobTitle">Software Engineer</span>
-	<span itemprop="nationality">VN</span>
-</div>
+<footer class="w-content">
+	<div>
+		<a href="keys/conradhoang.ssh.pub">
+			<abbr title="SHA256:1891d46e318031f42c4942b3a6a6886b2dc019c8f7d9e20a6949f00c0088d8e3"
+				>1891d4&hellip;</abbr
+			>
+			(SSH)
+		</a>
+		<br />
+		<a href="keys/conradhoang.gpg.pub">
+			<abbr title="MD5:7EEC55A3F4BEBF079E5F51CA6304382A6349D8B3">7EEC55&hellip;</abbr>
+			(GPG)
+		</a>
+	</div>
+	<p>Subscribe to RSS <a href="feed.xml">via RSS</a></p>
+
+	<strong>tcmhoang © 2024</strong>
+
+	<div class="hidden" aria-hidden="true" itemtype="http://schema.org/Person">
+		<span itemprop="gender">Male</span>
+		<span itemprop="email">tcmhoang@outlook.com</span>
+		<span itemprop="givenName">Tuan</span>
+		<span itemprop="familyName">Hoang</span>
+		<span itemprop="name">Tuan Minh Hoang</span>
+		<span itemprop="additionalName">Conrad Hoang</span>
+		<span itemprop="jobTitle">Software Engineer</span>
+		<span itemprop="nationality">VN</span>
+	</div>
+</footer>
+
+<style>
+	footer {
+		color: var(--text);
+		padding: 2rem 0;
+		max-width: 100%;
+		margin: 0 auto;
+		font-size: var(--step--1);
+		display: grid;
+		place-items: center;
+		row-gap: 0.5rem;
+	}
+
+	strong {
+		color: var(--sub-text);
+		font-size: var(--step--2);
+	}
+</style>
