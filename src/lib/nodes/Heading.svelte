baef10afcc2b0d4df6e0ed3d74f3b64c0b999c41
Conrad Hoang <tcmhoang@outlook.com>
2023-12-28 09:29:37 -0500

    add permalink to heading node

--- src/lib/nodes/Heading.svelte
+++ src/lib/nodes/Heading.svelte
@@ -11,22 +11,35 @@
 	let id = null;
 
 	onMount(() => {
-		if (element.firstElementChild?.textContent ?? false) {
-			id = (element.firstElementChild?.textContent ?? '')
-				.replace(/[?]/g, '')
-				.replace(/\s+/g, '-')
-				.toLowerCase();
+		if (element && element.childNodes) {
+			id =
+				[...element.childNodes]
+					.find((c) => c.nodeType == Node.TEXT_NODE)
+					?.textContent?.replace(
+						/[\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF|?]/g,
+						''
+					)
+					.trim()
+					.replace(/\s+/g, '-')
+					.toLowerCase() ?? null;
 		}
 	});
 </script>
 
-<svelte:element this={`h${level}`} bind:this={element}> <slot /> </svelte:element>
-<p>{id}</p>
+<svelte:element this={`h${level}`} bind:this={element} {id}>
+	<slot />
+	<a href="#{id}" title="permalink">permalink</a>
+</svelte:element>
 
 <style>
 	h2 {
 		font-weight: bold;
 		font-size: var(--step-2);
 		margin-bottom: 0.5rem;
+	}
+	a {
+		font-weight: bold;
+		color: var(--peach);
+		font-size: var(--step--2);
 	}
 </style>
