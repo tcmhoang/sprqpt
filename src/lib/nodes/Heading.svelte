2531ba7dd0d87d9866a28ffe22db41b8b938b811
Conrad Hoang <tcmhoang@outlook.com>
2024-01-07 11:53:15 -0500

    separate permalink from heading node fix not change style in iframe

--- src/lib/nodes/Heading.svelte
+++ src/lib/nodes/Heading.svelte
@@ -1,38 +1,10 @@
 <script>
-	import PaperClipIcon from '$lib/icons/PaperClipIcon.svelte';
-	import { onMount } from 'svelte';
-
 	/** @type number */
 	export let level;
-
-	const emojiRegex = /[\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF]|\?/g;
-
-	/** @type Element */
-	let element;
-
-	/** @type {{id?: string}} */
-	let props = {};
-
-	onMount(() => {
-		if (element && element.childNodes.length) {
-			const maybe_text_content = element.childNodes[0].textContent;
-
-			if (!emojiRegex.test(maybe_text_content ?? '')) {
-				props = {
-					id: maybe_text_content?.replace(emojiRegex, '').trim().replace(/\s+/g, '-').toLowerCase()
-				};
-			}
-		}
-	});
 </script>
 
-<svelte:element this={`h${level}`} bind:this={element} {...props}>
-	<span> <slot /> </span>
-	{#if props.id}
-		<a href="#{props.id}" title="permalink">
-			<PaperClipIcon />
-		</a>
-	{/if}
+<svelte:element this={`h${level}`}>
+	<slot />
 </svelte:element>
 
 <style lang="scss">
@@ -55,16 +27,5 @@
 		align-items: center;
 		margin-top: 1rem;
 		scroll-margin-top: 4rem;
-	}
-	a {
-		color: var(--text);
-		width: 2.75rem;
-		height: 2.75rem;
-		border-radius: 0.375rem;
-
-		&:hover {
-			color: var(--peach);
-			background-color: var(--p-surface);
-		}
 	}
 </style>
--- src/routes/(site)/(main)/Header/Banner.svelte
+++ src/routes/(site)/(main)/Header/Banner.svelte
@@ -13,29 +13,40 @@
 	/** @type HTMLIFrameElement */
 	let iframe;
 
-	/** @type {function(): void} */
-	let unsubscribe = () => {};
+	/** @type {(function(): void) | null} */
+	let unsubscribe = null;
 
-	onMount(() => {
-		const iframe_elem = iframe?.contentDocument?.querySelector('body');
+	const on_iframe_load = () => {
+		if (!unsubscribe) {
+			const iframe_elem = iframe?.contentDocument?.querySelector('body');
+			console.log(iframe_elem);
 
-		unsubscribe = theme.subscribe((s) => {
 			if (iframe_elem) {
-				switch (s) {
-					case 'light':
-						return onThemeChange(heroCss?.light ?? '', iframe_elem);
-					case 'dark':
-						return onThemeChange(heroCss?.dark ?? '', iframe_elem);
-					case 'system':
-						return onThemeChange(heroCss?.sys ?? '', iframe_elem);
-					default:
-						return;
-				}
+				unsubscribe = theme.subscribe((s) => {
+					switch (s) {
+						case 'light':
+							return onThemeChange(heroCss?.light ?? '', iframe_elem);
+						case 'dark':
+							return onThemeChange(heroCss?.dark ?? '', iframe_elem);
+						case 'system':
+							return onThemeChange(heroCss?.sys ?? '', iframe_elem);
+						default:
+							return;
+					}
+				});
 			}
-		});
+		}
+	};
+
+	// A hack to set css in iframe at runtime
+	onMount(() => {
+		const id_check_iframe = setInterval(() => {
+			console.log('checking');
+			unsubscribe ? clearInterval(id_check_iframe) : on_iframe_load();
+		}, 500);
 	});
 
-	onDestroy(unsubscribe);
+	onDestroy(() => (unsubscribe ? unsubscribe() : null));
 
 	function onThemeChange(/** @type {string} */ css, /** @type HTMLElement */ iframe_elem) {
 		if (iframe_elem) iframe_elem.style.cssText = css;
@@ -53,6 +64,7 @@
 			loading="lazy"
 			scrolling="no"
 			bind:this={iframe}
+			on:load={on_iframe_load}
 		/>
 	{/if}
 </div>
