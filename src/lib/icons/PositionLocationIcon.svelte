7c3dac791e21af31bd24fc6cbb9dfa6b1ceb2141
Conrad Hoang <tcmhoang@outlook.com>
2024-01-10 08:33:38 -0500

    feat: supports richer install UI

--- src/lib/icons/PositionLocationIcon.svelte
+++ src/lib/icons/PositionLocationIcon.svelte
@@ -1,5 +1,11 @@
 <svg role="presentation" aria-hidden="true" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-	<g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+	<g
+		stroke="currentColor"
+		fill="transparent"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+	>
 		<path d="M12 21c3.5-3.6 7-6.8 7-10.8 0-4-3.1-7.2-7-7.2s-7 3.2-7 7.2 3.5 7.2 7 10.8Z" />
 		<path d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
 	</g>
