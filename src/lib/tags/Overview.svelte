d315a0700818eaeb287b41d13e4e0c8a05cb19d3
Conrad Hoang <tcmhoang@outlook.com>
2024-02-06 09:14:57 -0500

    fix: y11n error with blogs images and add summary title for the blog

---
+++ src/lib/tags/Overview.svelte
@@ -0,0 +1,18 @@
+<summary>
+	<slot />
+</summary>
+
+<style lang="scss">
+	summary {
+		&::first-line {
+			text-transform: uppercase;
+			letter-spacing: 0.1rem;
+		}
+		&::first-letter {
+			font-size: var(--step-7);
+			font-weight: bold;
+			margin-right: 0.75rem;
+			float: left;
+		}
+	}
+</style>
--- src/lib/tags/Tags.svelte
+++ src/lib/tags/Tags.svelte
@@ -1,4 +1,5 @@
 <script context="module">
 	export { default as PermaLink } from './PermaLink.svelte';
 	export { default as LinkMe } from './LinkMe.svelte';
+	export { default as Overview } from './Overview.svelte';
 </script>
