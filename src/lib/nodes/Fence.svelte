333839d1c965c3fd165db37064fadbd1bdfaceb7
Conrad Hoang <tcmhoang@outlook.com>
2024-02-09 04:54:14 -0500

    fence with hljs

---
+++ src/lib/nodes/Fence.svelte
@@ -0,0 +1,36 @@
+<script>
+	import '@catppuccin/highlightjs/sass/catppuccin-mocha.scss';
+	import hljs from 'highlight.js/lib/core';
+	import shell from 'highlight.js/lib/languages/shell';
+
+	hljs.registerLanguage('sh', shell);
+
+	/** @type string */
+	export let content;
+	/** @type string */
+	export let language;
+	/** @type boolean */
+	export let process;
+
+	const result = process
+		? hljs.highlight(content, { language: language ?? 'sh' })
+		: { value: content };
+</script>
+
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+<pre><code class={`language-${language}`}>{@html result.value}</code></pre>
+
+<style>
+	pre {
+		padding: 1rem;
+		border-radius: 1rem;
+		overflow: auto;
+		margin: 0;
+	}
+
+	code {
+		font-size: var(--step--1);
+		font-weight: bold;
+		font-family: ui-monospace, monospace;
+	}
+</style>
